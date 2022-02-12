6858092de12611c2d820cbb37ec348e7f49b9cb8
Vonarian <ardeshir.nahavandi@gmail.com>
2022-02-12 04:27:11 +0330

    Auto-update fixed, del.bat fixed.

--- assets/del.bat
+++ assets/del.bat
@@ -10,8 +10,8 @@
 
 @echo Starting Server...
 
-start cmd.exe /c  "%~dp0\mona\MonaTiny.exe"
+start cmd.exe /c "%~dp0\mona\MonaTiny.exe"
 @timeout 1 /nobreak >NUL
 @echo Starting Stream...
 @timeout 2 /nobreak >NUL
-"%~dp0/ffmpeg.exe" -f gdigrab -framerate 45 -i desktop -c:v libx264 -b:v 2M -maxrate 4M -bufsize 3M -crf 18 -pix_fmt yuv420p -tune zerolatency -preset ultrafast -f flv rtmp://%NetworkIP%:1935
+"%~dp0ffmpeg.exe" -f gdigrab -framerate 45 -i desktop -c:v libx264 -b:v 2M -maxrate 4M -bufsize 3M -crf 18 -pix_fmt yuv420p -tune zerolatency -preset ultrafast -f flv rtmp://%NetworkIP%:1935
