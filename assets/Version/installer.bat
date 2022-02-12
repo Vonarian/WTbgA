5b7d7eea05ef2d0441782a272c5da8355bae7796
Vonarian <ardeshir.nahavandi@gmail.com>
2022-02-12 04:23:54 +0330

    Auto-update fixed, del.bat fixed.

--- assets/Version/installer.bat
+++ assets/Version/installer.bat
@@ -5,11 +5,12 @@ CD ..
 CD ..
 CD ..
 SET MNIST_DIR=%CD%
-POPD
 ECHO %MNIST_DIR%
   @timeout 2 /nobreak >NUL
   @echo Proceeding to update the application, please do not close the window!
 taskkill /F /IM wtbgassistant.exe
-@echo on
-powershell.exe Add-AppPackage -path %MNIST_DIR%\out\WTbgA.msix
-timeout 5
+@echo "%MNIST_DIR%\out\WTbgA.msix"
+powershell.exe Add-AppPackage -Path '%MNIST_DIR%\out\WTbgA.msix'
+@echo Installation process complete
+
+timeout 10
